b078bd4789848a88826144567207c1665b34bd6a
CaiquePrado <caiqueprado76@gmail.com>
2025-01-26 17:34:12 -0300

    feat: add product factory

---
+++ src/main/java/dto/Product.java
@@ -0,0 +1,23 @@
+package dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Product {
+    private Long id;
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private Double discountPercentage;
+    private Double rating;
+    private Integer stock;
+    private String brand;
+    private String category;
+    private String thumbnail;
+    private List<String> images;
+}
---
+++ src/main/java/factory/ProductFactory.java
@@ -0,0 +1,22 @@
+package factory;
+
+import java.math.BigDecimal;
+
+import dto.Product;
+
+public interface ProductFactory {
+
+    static Product validProductFactory(){
+        return Product.builder()
+        .title("Perfume Oil")
+        .description("Mega Discount, Impression of A...")
+        .price(BigDecimal.valueOf(13))
+        .discountPercentage(8.4)
+        .rating(4.26)
+        .stock(65)
+        .brand("Impression of Acqua Di Gio")
+        .category("fragrances")
+        .thumbnail("https://i.dummyjson.com/data/products/11/thumnail.jpg")
+        .build();
+    }
+}
