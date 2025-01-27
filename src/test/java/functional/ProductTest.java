b7378ba2d688dea9f2ce2ed9ddf432c7d0bb09c1
CaiquePrado <caiqueprado76@gmail.com>
2025-01-27 18:18:18 -0300

    test: add delete product test

--- src/test/java/functional/ProductTest.java
+++ src/test/java/functional/ProductTest.java
@@ -1,17 +1,36 @@
 package functional;
 
-import static org.apache.http.HttpStatus.SC_CREATED;
+import static org.apache.http.HttpStatus.*;
 
 import org.testng.annotations.Test;
+import org.testng.annotations.DataProvider;
 
 import client.BaseTest;
 
 public class ProductTest extends BaseTest {
 
-    @Test
+    @Test(description = "CT003.001")
     public void shouldCreateProduct(){
         dummyClient
         .createValidProduct()
         .statusCode(SC_CREATED);
+    }
+
+
+    @DataProvider(name = "productIds")
+    public Object[][] productIds() {
+        return new Object[][] {
+                { 1 },
+                { 2 },
+                { 3 }
+        };
+    }
+
+
+    @Test(description = "CT004.001")
+    public void shouldDeleteProduct(){
+        dummyClient
+        .deleteProductById()
+        .statusCode(SC_OK);
     }
 }
