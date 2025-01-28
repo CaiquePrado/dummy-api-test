61db45559593833102036c97bd42f09fe4734951
CaiquePrado <caiqueprado76@gmail.com>
2025-01-28 19:09:21 -0300

    test: add list all products test

--- src/test/java/smoke/ProductSmokeTest.java
+++ src/test/java/smoke/ProductSmokeTest.java
@@ -1,13 +1,12 @@
-package functional;
+package smoke;
 
 import static org.apache.http.HttpStatus.*;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.DataProvider;
 
 import client.BaseTest;
 
-public class ProductTest extends BaseTest {
+public class ProductSmokeTest extends BaseTest {
 
     @Test(description = "CT003.001")
     public void shouldCreateProduct(){
@@ -16,21 +15,17 @@ public class ProductTest extends BaseTest {
         .statusCode(SC_CREATED);
     }
 
-
-    @DataProvider(name = "productIds")
-    public Object[][] productIds() {
-        return new Object[][] {
-                { 1 },
-                { 2 },
-                { 3 }
-        };
-    }
-
-
     @Test(description = "CT004.001")
     public void shouldDeleteProduct(){
         dummyClient
         .deleteProductById()
+        .statusCode(SC_OK);
+    }
+
+    @Test(description = "CT004.001")
+    public void shouldListProducts(){
+        dummyClient
+        .listAllProducts()
         .statusCode(SC_OK);
     }
 }
