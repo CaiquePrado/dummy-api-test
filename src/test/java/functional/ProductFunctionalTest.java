066d64c8c31383e4c5f979970d285d3d084b91e9
CaiquePrado <caiqueprado76@gmail.com>
2025-02-03 16:18:20 -0300

    test: add create product functional test

---
+++ src/test/java/functional/ProductFunctionalTest.java
@@ -0,0 +1,34 @@
+package functional;
+
+import base.BaseTest;
+import dto.Product;
+import factory.ProductFactory;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.apache.http.HttpStatus.SC_CREATED;
+
+public class ProductFunctionalTest extends BaseTest {
+
+    @DataProvider(name = "productFactory")
+    public Object[][] productFactory() {
+        return new Object[][]{
+                { ProductFactory.createProductWithTitleFactory() },
+                { ProductFactory.createProductWithDescriptionFactory() },
+                { ProductFactory.createProductWithPriceFactory() },
+                { ProductFactory.createProductWithDiscountPercentageFactory() },
+                { ProductFactory.createProductWithRatingFactory() },
+                { ProductFactory.createProductWithStockFactory() },
+                { ProductFactory.createProductWithBrandFactory() },
+                { ProductFactory.createProductWithCategoryFactory() },
+                { ProductFactory.createProductWithThumbnailFactory() }
+        };
+    }
+
+    @Test(dataProvider = "productFactory")
+    public void shouldCreateProductWithOneAttributeTest(Product product){
+        dummyClient
+        .createValidProductOneAttribute(product)
+        .statusCode(SC_CREATED);
+    }
+}
