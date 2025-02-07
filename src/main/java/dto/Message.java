665227603b4f90cd0ff6da450b0b97540e5df6d5
CaiquePrado <caiqueprado76@gmail.com>
2025-02-07 14:30:20 -0300

    feat: add message dto

---
+++ src/main/java/dto/Message.java
@@ -0,0 +1,12 @@
+package dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Message {
+    private String message;
+}
